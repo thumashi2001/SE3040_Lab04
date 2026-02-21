b159dab5a82eaece2cb79c6ab81927df45679f7f
Thumashi <thumashinawanjana@gmail.com>
2026-02-21 15:26:58 +0530

    Add file read example

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Node is working!");
+const fs = require("fs");
+
+fs.readFile("file.txt", "utf8", (err, data) => {
+    if (err) {
+        console.log("Error reading file:", err);
+        return;
+    }
+    console.log("File content:");
+    console.log(data);
+});
